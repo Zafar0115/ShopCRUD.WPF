6dc2c8ae063434ef69961389e6a2b126f33c696b
Zafar0115 <zafarzoyirovzohidovich@gmail.com>
2023-09-18 17:36:43 -0400

    first commit

---
+++ ShopCRUD.Data/Data/ShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopCRUD.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCRUD.Data.Data
+{
+    public class ShopDbContext:DbContext
+    {
+        public DbSet<Shop> MyProperty { get; set; }
+    }
+}
